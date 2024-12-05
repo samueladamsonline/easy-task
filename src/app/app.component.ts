7c1cf35f7ea172fd83c217f5c7c99649c6d7c54b
Samuel Adams <samueladamsonline@gmail.com>
2024-12-04 19:02:32 -0800

    Enhance the type checking around the user find logic.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,9 +14,9 @@ import { TasksComponent } from "./tasks/tasks.component";
 export class AppComponent {
   users = DUMMY_USERS;
   selectedUserId: string = DUMMY_USERS[0].id;
-  
+
   get selectedUser() {
-    return this.users.find(user => user.id === this.selectedUserId)!;
+    return this.users.find(user => user.id === this.selectedUserId);
   }
 
   onSelectUser(id: string) {
